f8bb3ffdc9485ab1f85ddcd460ebbb4784e2a47f
Talha Seraj <60309954+talhaseraj@users.noreply.github.com>
2023-10-24 10:33:24 +0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/estisharati/party_wizard/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.estisharati.party_wizard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+class AppColors{
+  const AppColors();
+// color codes
+ static const primaryColor = Color(0xfff25410);
+
+}
